0a544e79fdbcf04abc4f39c33ddc307718658ca7
Radzi <imradzi@gmail.com>
2026-02-12 11:49:11 +0800

    fix gethreadindex

--- GetThreadIndex.cpp
+++ GetThreadIndex.cpp
@@ -7,8 +7,7 @@ std::size_t GetThreadIndex(const std::thread::id id) {
     static std::size_t nextindex = 0;
     static std::unordered_map<std::thread::id, std::size_t> ids;
     std::lock_guard<std::mutex> lock(my_mutex);
-    auto iter = ids.find(id);
-    if(iter == ids.end())
-        return ids[id] = nextindex++;
+    auto [iter, inserted] = ids.try_emplace(id, nextindex);
+    if (inserted) ++nextindex;
     return iter->second;
 }
