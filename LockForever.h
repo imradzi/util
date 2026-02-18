87c942b445c93b694e2657e06396f84c5c47a5a0
Radzi <imradzi@gmail.com>
2026-02-18 08:28:36 +0800

    change how to call LockForever and added TryLock

--- LockForever.h
+++ LockForever.h
@@ -5,17 +5,25 @@
 
 using namespace std::chrono_literals;
 
+//LockForever: while the check.first == check.second try to lock, else return fail.
 template<typename T>
-std::tuple<bool, std::unique_lock<T>> LockForever(T& lock, ObservableAtomic& isServerDown, const std::chrono::milliseconds timeOut = 500ms) {
+std::tuple<bool, std::unique_lock<T>> LockForever(T& lock, std::pair<ObservableAtomic *, bool> check, const std::chrono::milliseconds timeOut = 500ms) {
     // Create a sleeper that monitors the flag
-    WakeableSleeper sleeper {{{&isServerDown, true}}};
+    WakeableSleeper sleeper{{{check.first, !check.second}}};
 
-    while (!isServerDown.load()) {
+    while (check.first->load() == check.second) {
         if (lock.try_lock_for(timeOut)) {
             return {true, std::unique_lock<T>(lock, std::adopt_lock)};
         }
         // Sleep briefly, will wake early if isServerDown changes
         sleeper.sleep_for(std::chrono::milliseconds(10));
     }
+    return {false, std::unique_lock<T>()};
+}
+
+// helper functor to return lock that will be unlocked when going out of scope, and a bool to indicate if lock is acquired or not.
+template<typename T>
+std::tuple<bool, std::unique_lock<T>> TryLock(T& lock) {
+    if (lock.try_lock()) return {true, std::unique_lock<T>(lock, std::adopt_lock)};
     return {false, std::unique_lock<T>()};
 }
